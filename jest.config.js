9524b1f072c1e23624a1a9b049fc35c7f54779c1
Peter Matta <developer@mattapet.com>
2021-09-19 23:11:55 +0200

    chore: link dependencies

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,13 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  roots: ['<rootDir>/src'],
+  roots: [
+    '<rootDir>/packages/cats/core/src',
+    '<rootDir>/packages/cats/free/src',
+    '<rootDir>/packages/core/src',
+    '<rootDir>/packages/effect/core/src',
+    '<rootDir>/packages/effect/kernel/src',
+    '<rootDir>/packages/effect/std/src',
+  ],
   testEnvironment: 'node',
 };
-
