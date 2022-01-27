7a3c52b2e06753f6fb2da4dff71e482a3be9d21b
Peter Matta <developer@mattapet.com>
2022-01-27 11:08:16 +0100

    chore(http): add missing license headers

--- packages/http/__tests__/server/animal.ts
+++ packages/http/__tests__/server/animal.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import { Codable } from '@fp4ts/http-dsl-server';
 import { typeDef } from '@fp4ts/http-dsl-shared';
 import { Schema, TypeOf } from '@fp4ts/schema';
