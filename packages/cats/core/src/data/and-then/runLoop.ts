d68c5842eb6eedd8b31739fd3887a40256fb9b09
Peter Matta <developer@mattapet.com>
2022-02-02 14:30:09 +0100

    refactor(cats/core): unroll and-then runloop to loops

--- packages/cats/core/src/data/and-then/runLoop.ts
+++ packages/cats/core/src/data/and-then/runLoop.ts
@@ -3,31 +3,36 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import { tailrec } from '@fp4ts/core';
-import { AndThen, Concat, view, View } from './algebra';
+import { AndThen, Concat, View } from './algebra';
 
-export const runLoop_ = <A, B>(f: AndThen<A, B>, start: A): B => {
-  const loop = tailrec(<X>(self: View<X, B>, current: X): B => {
+export function runLoop_<A, B>(f: AndThen<A, B>, start: A): B {
+  let current: unknown = start;
+  let self: View<unknown, B> = f as View<unknown, B>;
+
+  while (true) {
     if (self.tag === 'single') return self.fun(current);
 
-    const leftV = view(self.left);
-    return leftV.tag === 'single'
-      ? loop(view(self.right), leftV.fun(current))
-      : loop(view(rotateAcc(leftV, self.right)), current);
-  });
+    const leftV = self.left as View<unknown, unknown>;
+    if (leftV.tag === 'single') {
+      self = self.right as View<unknown, B>;
+      current = leftV.fun(current);
+    } else {
+      self = rotateAcc(leftV, self.right) as View<unknown, B>;
+    }
+  }
+}
 
-  return loop(view(f), start);
-};
-
-const rotateAcc = <A, B, C>(
+function rotateAcc<A, B, C>(
   left0: AndThen<A, B>,
   right0: AndThen<B, C>,
-): AndThen<A, C> => {
-  const loop = tailrec(
-    <X>(left: View<A, X>, right: AndThen<X, C>): AndThen<A, C> =>
-      left.tag === 'concat'
-        ? loop(view(left.left), new Concat(left.right, right))
-        : new Concat(left, right),
-  );
-  return loop(view(left0), right0);
-};
+): AndThen<A, C> {
+  let left: View<A, unknown> = left0 as View<A, B>;
+  let right: AndThen<unknown, C> = right0 as AndThen<unknown, C>;
+
+  while (left.tag === 'concat') {
+    right = new Concat(left.right, right) as AndThen<unknown, C>;
+    left = left.left as View<A, unknown>;
+  }
+
+  return new Concat(left, right);
+}
