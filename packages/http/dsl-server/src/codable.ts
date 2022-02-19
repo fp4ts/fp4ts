05b03b651b0149cbfdd7c30c730de45519e71d6f
Peter Matta <developer@mattapet.com>
2022-02-19 22:33:39 +0100

    feat(http/dsl-server): use Decoder in the codable derivation

--- packages/http/dsl-server/src/codable.ts
+++ packages/http/dsl-server/src/codable.ts
@@ -4,9 +4,9 @@
 // LICENSE file in the root directory of this source tree.
 
 import { $type, TyK, TyVar } from '@fp4ts/core';
-import { Either, EitherT, Identity, Try } from '@fp4ts/cats';
+import { Either, EitherT, Eval, Try } from '@fp4ts/cats';
 import { MessageFailure, ParsingFailure } from '@fp4ts/http-core';
-import { DecoderT, Encoder, Schema, DecodeFailure } from '@fp4ts/schema';
+import { Encoder, Schema, DecodeFailure, Decoder } from '@fp4ts/schema';
 
 export interface Codable<A> {
   encode: (a: A) => string;
@@ -16,19 +16,19 @@ export interface Codable<A> {
 export const Codable = Object.freeze({
   json: {
     fromSchema<A>(sa: Schema<A>): Codable<A> {
-      const decoder = DecoderT.string(Identity.Monad)
-        .andThen(Identity.Monad)(
-          DecoderT(s =>
+      const decoder = Decoder.string
+        .andThen(
+          Decoder(s =>
             EitherT(
-              Try(() => JSON.parse(s)).toEither.leftMap(
-                e => new DecodeFailure(e.message),
+              Eval.delay(() =>
+                Try(() => JSON.parse(s)).toEither.leftMap(
+                  e => new DecodeFailure(e.message),
+                ),
               ),
             ),
           ),
         )
-        .andThen(Identity.Monad)(
-        sa.interpret(DecoderT.Schemable(Identity.Monad)),
-      );
+        .andThen(sa.interpret(Decoder.Schemable));
       const encoder = sa
         .interpret(Encoder.Schemable)
         .map(a => JSON.stringify(a));
@@ -36,7 +36,9 @@ export const Codable = Object.freeze({
       return {
         encode: a => encoder.encode(a),
         decode: s =>
-          decoder.decode(s).value.leftMap(f => new ParsingFailure(f.message)),
+          decoder
+            .decode(s)
+            .value.value.leftMap(f => new ParsingFailure(f.message)),
       };
     },
   },
