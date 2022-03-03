11446ed95f5ce1febef4497dd679115decf5932d
Peter Matta <developer@mattapet.com>
2022-03-03 15:54:45 +0100

    chore(effect/core): add missing license headers

--- packages/effect/core/src/internal/io-constants.ts
+++ packages/effect/core/src/internal/io-constants.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 export const MapK = 0;
 export const FlatMapK = 1;
 export const HandleErrorWithK = 2;
