5f4dffb8ef41b5efb538d670faf37933abc8cb66
Peter Matta <developer@mattapet.com>
2022-03-06 10:11:38 +0100

    chore: add missing license headers

--- packages/http/__tests__/dsl-client/success.spec.ts
+++ packages/http/__tests__/dsl-client/success.spec.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import fc from 'fast-check';
 import { Left, List, None, NonEmptyList, Some } from '@fp4ts/cats';
 import { IO, Resource } from '@fp4ts/effect';
