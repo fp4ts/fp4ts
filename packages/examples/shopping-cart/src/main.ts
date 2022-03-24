dd7bdf84d63cd8e07a51396a973a6259925a029d
Peter Matta <developer@mattapet.com>
2022-03-24 21:09:04 +0100

    feat(examples/shopping-cart): add http logger

--- packages/examples/shopping-cart/src/main.ts
+++ packages/examples/shopping-cart/src/main.ts
@@ -3,20 +3,34 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import { IO, unsafeRunMain } from '@fp4ts/effect';
+import { pipe } from '@fp4ts/core';
+import { FunctionK } from '@fp4ts/cats';
+import { Console, IO, IOF, unsafeRunMain } from '@fp4ts/effect';
+import { ConsoleLogger, LogFormat, TimestampLogger } from '@fp4ts/logging';
+import { HttpLogger } from '@fp4ts/http-server';
 import { NodeServerBuilder } from '@fp4ts/http-node-server';
 
 import { IOAppModule } from './modules/infrastructure';
 
+const F = IO.Async;
+const logger = HttpLogger(F)<IOF, IOF>(
+  pipe(
+    ConsoleLogger(F, Console.make(F)).format(LogFormat.default<string>()),
+    TimestampLogger(F, F),
+  ),
+  FunctionK.id<IOF>(),
+);
+const middleware = logger;
+
 function main(): void {
   unsafeRunMain(
-    IO.Monad.do(function* (_) {
+    F.do(function* (_) {
       const appModule = yield* _(IOAppModule.make());
 
       yield* _(
-        NodeServerBuilder.make(IO.Async)
+        NodeServerBuilder.make(F)
           .bindLocal(3000)
-          .withHttpApp(appModule.apiService.toHttpApp)
+          .withHttpApp(middleware(appModule.apiService.toHttpApp))
           .serve()
           .compileConcurrent().last,
       );
