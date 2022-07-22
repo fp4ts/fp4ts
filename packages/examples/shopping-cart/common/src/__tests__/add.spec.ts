34ed7f9a0a0eed68010e14235268ffee827ae7ae
Peter Matta <developer@mattapet.com>
2022-07-22 08:58:43 +0200

    chore: setup shopping cart packages

---
+++ packages/examples/shopping-cart/common/src/__tests__/add.spec.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+import { add } from '../add';
+
+it('should add two numbers correctly', () => {
+  expect(add(2, 2)).toBe(4);
+});
---
+++ packages/examples/shopping-cart/common/src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+/**
+ * @module @shopping-cart/common
+ */
+export * from './add';
