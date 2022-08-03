72f12ce0b30a0e1fe346c5b669e27ae413dd27bd
Peter Matta <developer@mattapet.com>
2022-08-03 14:23:26 +0200

    feat(sql): a pg lib wrapper

---
+++ packages/sql/pg/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+/**
+ * @module sql/pg
+ */
+export * from './client';
+export * from './client-base';
+export * from './pool';
