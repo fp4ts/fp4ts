b7471ffe4a728d3e52db5be27dd309e0be4d7eae
Peter Matta <developer@mattapet.com>
2022-09-05 11:42:52 +0200

    chore: set all versions to 0.0.0

--- packages/optics/perf/src/traversable.perf.ts
+++ packages/optics/perf/src/traversable.perf.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2021-2022 Peter Matta
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import { focus } from '@fp4ts/optics-core';
 import { add, configure, cycle, suite } from 'benny';
 
