fd844961c5bf32ece9d62c815fb891f6e49271de
Peter Matta <developer@mattapet.com>
2023-01-26 09:45:04 +0100

    fix(cats/core): remove unused array import

--- packages/cats/core/src/data/collections/chain/constructors.ts
+++ packages/cats/core/src/data/collections/chain/constructors.ts
@@ -3,7 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import '../array/array';
 import { Kind } from '@fp4ts/core';
 import { Foldable } from '../../../foldable';
 import { Either } from '../../either';
