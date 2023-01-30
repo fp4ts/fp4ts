26b9be8718b7fec010cfeafab10ab3e3b8c8e796
Peter Matta <developer@mattapet.com>
2023-01-30 11:29:14 +0100

    refactor(cats/core): AndThen reuse instances in runloop

--- packages/cats/core/src/data/and-then/algebra.ts
+++ packages/cats/core/src/data/and-then/algebra.ts
@@ -3,8 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import { runLoop_ } from './runLoop';
-
 const AndThenTag = Symbol('@fp4ts/cats/core/and-then');
 
 export abstract class AndThen<in A, out B> {
@@ -22,7 +20,7 @@ export interface Single<A, B> extends AndThen<A, B> {
 
 export function Single<A, B>(fun: (a: A) => B, idx: number): Single<A, B> {
   const apply = function (this: AndThen<A, B>, a: A): B {
-    return runLoop_({ tag: 'single', fun, idx } as any, a);
+    return runLoop_(apply as any, a);
   };
 
   (apply as any)[AndThenTag] = true;
@@ -45,7 +43,7 @@ export function Concat<A, E, B>(
   right: AndThen<E, B>,
 ): Concat<A, E, B> {
   const apply = function (this: AndThen<A, B>, a: A): B {
-    return runLoop_({ tag: 'concat', left, right } as any, a);
+    return runLoop_(apply as any, a);
   };
 
   (apply as any)[AndThenTag] = true;
@@ -63,3 +61,28 @@ export function isAndThen<A, B>(f: (a: A) => B): f is AndThen<A, B> {
 
 export type View<A, B> = Single<A, B> | Concat<A, any, B>;
 export const view = <A, B>(_: AndThen<A, B>): View<A, B> => _ as any;
+
+function runLoop_<A, B>(f: AndThen<A, B>, start: A): B {
+  let current: unknown = start;
+  let self: View<unknown, unknown> = f as View<unknown, unknown>;
+  const stack: AndThen<unknown, unknown>[] = [];
+
+  while (true) {
+    while (self.tag !== 'single') {
+      const leftV = self.left as View<unknown, unknown>;
+      const rightV = self.right as View<unknown, unknown>;
+
+      if (leftV.tag === 'single') {
+        self = rightV as View<unknown, unknown>;
+        current = leftV.fun(current);
+      } else {
+        stack.push(rightV);
+        self = leftV;
+      }
+    }
+
+    current = self.fun(current);
+    if (stack.length === 0) return current as B;
+    self = stack.pop()! as View<unknown, unknown>;
+  }
+}
--- packages/cats/core/src/data/and-then/operators.ts
+++ packages/cats/core/src/data/and-then/operators.ts
@@ -3,7 +3,7 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import { AndThen, Concat, isAndThen, Single, view } from './algebra';
+import { AndThen, Concat, isAndThen, Single, View, view } from './algebra';
 import { fusionMaxStackDepth } from './consts';
 
 export const andThen: <E, B>(
@@ -21,19 +21,19 @@ export const andThen_ = <A, E, B>(
   g: (e: E) => B,
 ): AndThen<A, B> => {
   if (isAndThen(g)) return _andThen(f, g);
-  const fv = view(f);
+  const fv = f as View<A, E>;
   if (fv.tag === 'single') {
-    if (fv.idx < fusionMaxStackDepth)
-      return Single(x => g(fv.fun(x)), fv.idx + 1);
-    return Concat(f, Single(g, 0));
+    return fv.idx < fusionMaxStackDepth
+      ? Single(x => g(fv.fun(x)), fv.idx + 1)
+      : Concat(f, Single(g, 0));
   } else {
-    const rv = view(fv.right);
-    if (rv.tag === 'single' && rv.idx < fusionMaxStackDepth)
-      return Concat(
-        fv.left,
-        Single(x => g(rv.fun(x)), rv.idx + 1),
-      );
-    return Concat(f, Single(g, 0));
+    const rv = fv.right as View<any, E>;
+    return rv.tag === 'single' && rv.idx < fusionMaxStackDepth
+      ? Concat(
+          fv.left,
+          Single(x => g(rv.fun(x)), rv.idx + 1),
+        )
+      : Concat(f, Single(g, 0));
   }
 };
 
@@ -44,17 +44,17 @@ export const compose_ = <A, E, B>(
   if (isAndThen(f)) return _andThen(f, g);
   const gv = view(g);
   if (gv.tag === 'single') {
-    if (gv.idx < fusionMaxStackDepth)
-      return Single(x => gv.fun(f(x)), gv.idx + 1);
-    return Concat(Single(f, 0), g);
+    return gv.idx < fusionMaxStackDepth
+      ? Single(x => gv.fun(f(x)), gv.idx + 1)
+      : Concat(Single(f, 0), g);
   } else {
     const lv = view(gv.left);
-    if (lv.tag === 'single' && lv.idx < fusionMaxStackDepth)
-      return Concat(
-        Single(x => lv.fun(f(x)), lv.idx + 1),
-        gv.right,
-      );
-    return Concat(Single(f, 0), g);
+    return lv.tag === 'single' && lv.idx < fusionMaxStackDepth
+      ? Concat(
+          Single(x => lv.fun(f(x)), lv.idx + 1),
+          gv.right,
+        )
+      : Concat(Single(f, 0), g);
   }
 };
 
@@ -63,35 +63,37 @@ const _andThen = <A, B, C>(
   ab: AndThen<A, B>,
   bc: AndThen<B, C>,
 ): AndThen<A, C> => {
-  const fv = view(ab);
-  const gv = view(bc);
+  const fv = ab as View<A, B>;
+  const gv = bc as View<B, C>;
   if (fv.tag === 'single') {
-    const [f, indexF] = [fv.fun, fv.idx];
+    const f = fv.fun;
+    const indexF = fv.idx;
     if (gv.tag === 'single') {
-      const [g, indexG] = [gv.fun, gv.idx];
+      const g = gv.fun;
+      const indexG = gv.idx;
 
       return indexF + indexG < fusionMaxStackDepth
         ? Single(x => g(f(x)), indexF + indexG)
         : Concat(ab, bc);
     } else {
-      const leftV = view(gv.left);
-      if (leftV.tag === 'single' && indexF + leftV.idx < fusionMaxStackDepth) {
-        return Concat(
-          Single(x => leftV.fun(f(x)), indexF + leftV.idx),
-          gv.right,
-        );
-      } else {
-        return Concat(ab, bc);
-      }
+      const leftV = gv.left as View<B, any>;
+      return leftV.tag === 'single' && indexF + leftV.idx < fusionMaxStackDepth
+        ? Concat(
+            Single(x => leftV.fun(f(x)), indexF + leftV.idx),
+            gv.right,
+          )
+        : Concat(ab, bc);
     }
   }
 
-  const frv = view(fv.right);
+  const frv = fv.right as View<any, B>;
   if (frv.tag === 'single') {
-    const [f, indexF] = [frv.fun, frv.idx];
-    const gv = view(bc);
+    const f = frv.fun;
+    const indexF = frv.idx;
+    const gv = bc as View<B, C>;
     if (gv.tag === 'single') {
-      const [g, indexG] = [gv.fun, gv.idx];
+      const g = gv.fun;
+      const indexG = gv.idx;
       return indexF + indexG < fusionMaxStackDepth
         ? Concat(
             fv.left,
@@ -99,18 +101,16 @@ const _andThen = <A, B, C>(
           )
         : Concat(ab, bc);
     } else {
-      const leftV = view(gv.left);
-      if (leftV.tag === 'single' && indexF + leftV.idx < fusionMaxStackDepth) {
-        return Concat(
-          fv.left,
-          Concat(
-            Single(x => leftV.fun(f(x)), indexF + leftV.idx),
-            gv.right,
-          ),
-        );
-      } else {
-        return Concat(ab, bc);
-      }
+      const leftV = gv.left as View<B, any>;
+      return leftV.tag === 'single' && indexF + leftV.idx < fusionMaxStackDepth
+        ? Concat(
+            fv.left,
+            Concat(
+              Single(x => leftV.fun(f(x)), indexF + leftV.idx),
+              gv.right,
+            ),
+          )
+        : Concat(ab, bc);
     }
   } else {
     return Concat(ab, bc);
