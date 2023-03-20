61442d19955c9feaa4ae4c9794e416ef9e596505
Peter Matta <developer@mattapet.com>
2023-03-20 14:40:30 +0100

    refactor(cats/core/data): Backwards to use cosequenceEval from underlying TraverseStrategy

--- packages/cats/core/src/data/backwards.ts
+++ packages/cats/core/src/data/backwards.ts
@@ -3,13 +3,11 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import { id, Kind } from '@fp4ts/core';
+import { cached, Kind } from '@fp4ts/core';
 import { Functor } from '../functor';
 import { Applicative } from '../applicative';
 import { Monad } from '../monad';
 import { Contravariant } from '../contravariant';
-import { isDefer } from '../defer';
-import { TraverseStrategy } from '../apply';
 
 export type Backwards<F, A> = Kind<F, [A]>;
 
@@ -27,44 +25,34 @@ interface BackwardsObj {
 
 Backwards.Functor = F => Functor.of({ map_: F.map_ });
 Backwards.Contravariant = F => Contravariant.of({ contramap_: F.contramap_ });
-Backwards.Applicative = <F>(F: Applicative<F>) => {
-  const self = Applicative.of({
+Backwards.Applicative = cached(<F>(F: Applicative<F>) =>
+  Applicative.of({
     ...Backwards.Functor(F),
     pure: F.pure,
     ap_: (ff, fa) => F.map2_(fa, ff, (a, f) => f(a)),
     map2_: (fa, fb, f) => F.map2_(fb, fa, (b, a) => f(a, b)),
     map2Eval_: (fa, efb, f) =>
       efb.map(fb => F.map2_(fb, fa, (b, a) => f(a, b))),
-  });
 
-  if (!isDefer(F)) {
-    self.TraverseStrategy = use =>
-      F.TraverseStrategy(Rhs =>
+    TraverseStrategy: use =>
+      F.TraverseStrategy(T =>
         use({
-          defer: Rhs.defer,
-          map: Rhs.map,
-          map2: (fa, fb, f) => Rhs.map2(fb, fa, (b, a) => f(a, b)),
-          toG: Rhs.toG,
-          toRhs: Rhs.toRhs,
+          defer: T.defer,
+          map: T.map,
+          map2: (fa, fb, f) => T.map2(fb, fa, (b, a) => f(a, b)),
+          toG: T.toG,
+          toRhs: T.toRhs,
+          cosequenceEval: T.cosequenceEval,
         }),
-      );
-  } else {
-    const ts: TraverseStrategy<F, F> = {
-      defer: F.defer,
-      map: F.map_,
-      map2: (fa, fb, f) => F.map2_(fb, fa, (b, a) => f(a, b)),
-      toG: id,
-      toRhs: F.defer,
-    };
-    self.TraverseStrategy = use => use(ts);
-  }
+      ),
+  }),
+);
 
-  return self;
-};
-Backwards.Monad = <F>(F: Monad<F>) =>
+Backwards.Monad = cached(<F>(F: Monad<F>) =>
   Monad.of({
     flatMap_: F.flatMap_,
     tailRecM_: F.tailRecM_,
     // override all applicative methods with inverted application
     ...Backwards.Applicative(F),
-  });
+  }),
+);
